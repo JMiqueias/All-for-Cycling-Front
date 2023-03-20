67f1d4d68171401ad9ec77a39a273344dfdc2fa7
JMiqueias <araujo.miqueias520@gmail.com>
2023-03-20 17:46:44 -0300

    att

--- src/views/scripts/scrpValCadItem.js
+++ src/views/scripts/scrpValCadItem.js
@@ -1,13 +1,12 @@
-//import Cadastro from './cadastros.js';
 
-const form = document.getElementById('form');
 const campos = document.querySelectorAll('.required');
 const spans = document.querySelectorAll('.span-required');
 const image = document.getElementById('myFile');
+
 if (!token) {
   window.location.href = '/login';
 }
- fetch('https://api-centraldosom.onrender.com/auth', {
+ fetch('https://api-all-for-cycling.onrender.com/auth', {
    method: 'GET',
    headers: {
      'Authorization': 'Bearer ' + token
@@ -17,57 +16,6 @@ if (!token) {
          res.json().then(data => {
            console.log(data.message);
          })
-            form.addEventListener('submit',(event)=>{
-              event.preventDefault();
-              nameValidate();
-              cateValidade();
-              condValidade();
-              descValidate();
-              valValidate();
-              imgValidate();
-
-              let isValid = true;
-              for(let i = 0; i < campos.length; i++){
-                if(campos[i].style.border == '2px solid rgb(230, 54, 54)'){
-                  isValid = false;
-                  break;
-                }
-              }
-            console.log(isValid)
-              if(isValid){
-              var token = localStorage.getItem("token");
-              const formData = new FormData();
-              formData.append('nome', campos[0].value);
-              formData.append('categoria', campos[1].value);
-              formData.append('tipo', campos[2].value);
-              formData.append('descricao', campos[3].value);
-              formData.append('valor', campos[4].value);
-              formData.append('file', image.files[0]);
-              fetch('https://api-centraldosom.onrender.com/item', {
-                  method: 'POST',
-                  headers: {
-                    'Authorization': 'Bearer ' + token
-                  },
-                  body: formData
-                }).then(res => {
-                      if (res.status === 201) {
-                        res.json().then(data => {
-                          console.log(data.message);
-                        })
-                        window.location.href = '/produtos';
-                
-                      } else if (res.status === 400 || 500) {
-                        res.json().then(data => {
-                          console.log(data.message);
-                        })
-                        spans[6].style.display = 'block';
-                        //window.location.href = '/CadUser';
-                      }
-                    
-                    })
-                
-              } 
-            });
 } else if (res.status === 400 || 500) {
   res.json().then(data => {
     console.log(data.message);
@@ -78,19 +26,72 @@ if (!token) {
 
 });
 
+function cadastrar(){
+  nameValidate();
+  cateValidade();
+  condValidade();
+  descValidate();
+  valValidate();
+  imgValidate();
+
+  let isValid = true;
+  for (let i = 0; i < spans.length; i++) {
+    let span = spans[i].style.display
+    console.log(span)
+    if ( span == 'block') {
+      
+      isValid = false;
+      break;
+    }
+  }
+console.log(isValid)
+  if(isValid){
+  var token = localStorage.getItem("token");
+  const formData = new FormData();
+  formData.append('nome', campos[0].value);
+  formData.append('categoria', campos[1].value);
+  formData.append('tipo', campos[2].value);
+  formData.append('descricao', campos[3].value);
+  formData.append('valor', campos[4].value);
+  formData.append('file', image.files[0]);
+  fetch('https://api-all-for-cycling.onrender.com/item', {
+      method: 'POST',
+      headers: {
+        'Authorization': 'Bearer ' + token
+      },
+      body: formData
+    }).then(res => {
+          if (res.status === 201) {
+            res.json().then(data => {
+              console.log(data.message);
+            })
+            window.location.href = '/produtos';
+    
+          } else if (res.status === 400 || 500) {
+            res.json().then(data => {
+              console.log(data.message);
+            })
+            spans[6].style.display = 'block';
+            //window.location.href = '/CadUser';
+          }
+        
+        })
+    
+  } 
+};
 
 function setError(index){
-  campos[index].style.border = '2px solid #e63636';
+  //campos[index].style.border = '2px solid #e63636';
   spans[index].style.display = 'block';
 }
 
 function removError(index){
-  campos[index].style.border = '';
+  //campos[index].style.border = '';
   spans[index].style.display = 'none';
 }
 
 function nameValidate(){
-  if (campos[0].value.length < 5){
+  if (campos[0].value.length < 3){
     setError(0);
   }
   else{
